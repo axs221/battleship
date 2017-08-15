7b891543f2b0388cf9deb9665657e74c7611886b
Mike Cousins <mike@mikecousins.com>
2017-08-15 14:01:34 -0600

    Adding some turn change tests

--- src/js/__tests__/GameStore-test.js
+++ src/js/__tests__/GameStore-test.js
@@ -20,6 +20,20 @@ describe('A suite', () => {
     expect(gameState.phase).toBe('play');
   });
 
+  it('should handle enemy turn change properly', () => {
+    GameStore.setEnemyTurn();
+    const gameState = GameStore.getGameState();
+    expect(gameState.myTurn).toBe(false);
+    expect(gameState.enemy.board[0][0].clickable).toBe(false);
+  });
+
+  it('should handle my turn change properly', () => {
+    GameStore.setMyTurn();
+    const gameState = GameStore.getGameState();
+    expect(gameState.myTurn).toBe(true);
+    expect(gameState.enemy.board[0][0].clickable).toBe(true);
+  });
+
   it('should start finish phase properly', () => {
     GameStore.startFinishPhase(true);
     const gameState = GameStore.getGameState();
